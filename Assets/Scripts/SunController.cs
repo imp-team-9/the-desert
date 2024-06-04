7a5ddce92e9728abbfabe9f6319f6551e8f44633
parksb <parkgds@gmail.com>
2024-06-04 21:53:56 +0900

    Show day count as text UI

--- Assets/Scripts/SunController.cs
+++ Assets/Scripts/SunController.cs
@@ -1,11 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class SunController : MonoBehaviour
 {
     private float day = 2f;
     private float night = 10f;
+
+    private bool isDay = true;
+    private int dayCount = 1;
+    [SerializeField] private TMP_Text dayCountText;
+    [SerializeField] private AudioSource dayBell;
 
     // Start is called before the first frame update
     void Start()
@@ -21,13 +27,22 @@ public class SunController : MonoBehaviour
         if (transform.position.y >= -1.4)
         {
             speed = day;
+            if (!isDay)
+            {
+                isDay = true;
+                dayBell.Play();
+                dayCount += 1;
+                dayCountText.text = "Day " + dayCount;
+            }
+        } else {
+            isDay = false;
         }
 
         transform.RotateAround(Vector3.zero, Vector3.right, speed * Time.deltaTime);
         transform.LookAt(Vector3.zero);
     }
 
-    public bool isDay()
+    public bool IsDay()
     {
         return transform.position.y >= -1.4;
     }
